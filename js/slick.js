513d7d7c4bb166f0ba16f869ecbcf2c81a1cdcf4
olesyamaniuk <maniukolesya222@gmail.com>
2025-01-23 15:45:38 +0200

    2

--- js/slick.js
+++ js/slick.js
@@ -9,29 +9,11 @@ $(function () {
     autoplay: true,
     autoplaySpeed: 3500,
     speed: 1000,
-    dots: false,
+    dots: true,
     centerMode: true,
     cssEase: 'linear',
     prevArrow: '.gallery-list-left',
     nextArrow: '.gallery-list-right',
-  });
-});
-
-
-$(function () {
-  $('.reviews-list').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    variableWidth: true,
-    autoplay: true,
-    autoplaySpeed: 3500,
-    speed: 1000,
-    dots: true,
-    centerMode: true,
-    cssEase: 'linear',
-    prevArrow: '.reviews-list-left',
-    nextArrow: '.reviews-list-right',
   });
 });
 
